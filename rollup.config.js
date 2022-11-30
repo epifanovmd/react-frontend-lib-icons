3321be2a0e18c1a4c514f627951d1c612b0fda39
Андрей Епифанов <epifanovmd@gmail.com>
2022-11-30 20:01:13 +0300

    generate icons

--- rollup.config.js
+++ rollup.config.js
@@ -2,59 +2,65 @@ import babel from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import external from "rollup-plugin-peer-deps-external";
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 import autoprefixer from "autoprefixer";
 import postcss from "rollup-plugin-postcss";
 import reactSvg from "rollup-plugin-react-svg";
 
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: "lib/index.cjs.js",
+const plugins = [
+  external(),
+  babel({
+    babelHelpers: "bundled",
+    exclude: "node_modules/**",
+    plugins: ["external-helpers", "babel-plugin-styled-components"],
+  }),
+  resolve(),
+  commonjs(),
+  postcss({
+    extensions: ["css", "scss"],
+    use: {
+      sass: true,
+    },
+    plugins: [autoprefixer],
+  }),
+  terser(),
+  reactSvg({
+    svgo: {
+      plugins: [],
+      multipass: true,
+    },
+    jsx: false,
+    include: null,
+    exclude: null,
+  }),
+];
+
+export default [
+  {
+    input: "src/index.ts",
+    output: {
+      dir: "lib/cjs",
       format: "cjs",
-      sourcemap: true,
+      preserveModules: true,
     },
-    {
-      file: "lib/index.mjs.js",
+    external: [/node_modules/],
+    plugins: [
+      ...plugins,
+      typescript({ tsconfig: "tsconfig.cjs.json", sourceMap: false }),
+    ],
+  },
+  {
+    input: "src/index.ts",
+    output: {
+      dir: "lib/esm",
       format: "es",
-      sourcemap: true,
+      preserveModules: true,
     },
-  ],
-  external: [/node_modules/],
-  plugins: [
-    external(),
-    babel({
-      exclude: "node_modules/**",
-      plugins: ["external-helpers", "babel-plugin-styled-components"],
-    }),
-    resolve(),
-    commonjs(),
-    postcss({
-      extensions: ["css", "scss"],
-      use: {
-        sass: true,
-      },
-      plugins: [autoprefixer],
-    }),
-    terser(),
-    reactSvg({
-      // svgo options
-      svgo: {
-        plugins: [], // passed to svgo
-        multipass: true,
-      },
-
-      // whether to output jsx
-      jsx: false,
-
-      // include: string
-      include: null,
-
-      // exclude: string
-      exclude: null,
-    }),
-    typescript(),
-  ],
-};
+    external: [/node_modules/],
+    plugins: [
+      ...plugins,
+      typescript({ tsconfig: "tsconfig.mjs.json", sourceMap: false }),
+    ],
+  },
+];
